1a0c0f28201f3c40d3ba2cd7714eb09fb12bce05
Tyler Huffman <ty.huff24@gmail.com>
2020-07-29 17:58:00 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+require'pry'
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  new_arr = []
+  array.collect do |word|
+    new_arr.push(word.reverse)
+  end
+  new_arr.join(" ")
+end
