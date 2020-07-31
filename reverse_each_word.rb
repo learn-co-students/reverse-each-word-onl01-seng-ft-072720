f823f243198f6e406bfa5b224b4bd07c2e5a8b56
Nerly Ton <nerlyton@gmail.com>
2020-07-31 01:30:21 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,8 @@ require "pry"
 #   #reverse_each_word
 # end
 
-def reverse_each_word(sentence1)
-  new_sentence = sentence1.split(" ")
+def reverse_each_word(sentence)
+  new_sentence = sentence.split
   sentence2 = []
   new_sentence.collect do |sent|
     sentence2.push(sent.reverse)
